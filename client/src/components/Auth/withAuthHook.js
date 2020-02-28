97313ed41e760c9887011a5f3b0e73964bc5820e
Steve Mullins <sgmullins@gmail.com>
2020-02-28 14:40:01 -0700

    refactor schema files (#5)
    Split the schemas into 3 files

--- client/src/components/Auth/withAuthHook.js
+++ client/src/components/Auth/withAuthHook.js
@@ -6,5 +6,8 @@ import { GET_CURRENT_USER } from '../../queries/index';
 export const withAuthHook = conditionFunc => Component => props => {
   const { loading, data, error } = useQuery(GET_CURRENT_USER);
   if (loading) return null;
+  if (error) {
+    console.log(`Error from withAuth ${error}`);
+  }
   return conditionFunc(data) ? <Component {...props} /> : <Redirect to='/' />;
 };
---
+++ schemas/userSchema.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const userSchema = gql`
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    joinDate: String
+    favorites: [Meal]
+    purchases: [Meal]
+    allergies: [String]
+  }
+
+  type Token {
+    token: String!
+  }
+
+  extend type Query {
+    getCurrentUser: User
+  }
+
+  extend type Mutation {
+    loginUser(username: String!, password: String!): Token
+
+    createUser(username: String!, email: String!, password: String!): Token
+  }
+`;
+
+module.exports = userSchema;
